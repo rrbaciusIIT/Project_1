2e0ac1933d52aeaf7aef8a5acf3b5afc97480e98
Robert Bacius <rbacius@hawk.iit.edu>
2019-04-16 19:00:03 -0500

    adding JS/mobile friendly

--- JS/basicmap.js
+++ JS/basicmap.js
@@ -1,7 +1,7 @@
 function initMap(){
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.874684, lng: -87.635248},
-        zoom: 11    });
+        zoom: 12    });
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
